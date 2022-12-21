84ab5e88f6f6d4b1415e13b00b65eba6d1b10ae4
Billy Alexander de la Cruz Duque <alexander.cruz.duque@gmail.com>
2022-12-21 01:01:58 -0600

    Docker en Entorno de Desarrollo

--- db/Script DB.sql
+++ db/Script DB.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS db;
 
+show tables;
+
 USE db;
 
 CREATE TABLE IF NOT EXISTS `empleados`
@@ -9,6 +11,21 @@ CREATE TABLE IF NOT EXISTS `empleados`
     salario INT NOT NULL,
     primary key (id)
 );
+
+CREATE TABLE IF NOT EXISTS `usuarios`
+(
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    estado BOOLEAN DEFAULT 1,
+    primary key (id)
+);
+
+
+
+select * from usuarios;
+
 
 select * from empleados;
 

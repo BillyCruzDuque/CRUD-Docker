0dd38966c23780b0936a3e36cfa1c6254b908079
Billy Alexander de la Cruz Duque <alexander.cruz.duque@gmail.com>
2022-12-26 23:54:00 -0600

    Laboratorio1

--- src/routes/laboratorio.js
+++ src/routes/laboratorio.js
@@ -1,5 +1,5 @@
 import Router from 'express';
-import {postTipoMuestra} from "../controllers/laboratorio.controller.js";
+import {getTipoMuestra, postTipoMuestra} from "../controllers/laboratorio.controller.js";
 
 
 const router = Router();
